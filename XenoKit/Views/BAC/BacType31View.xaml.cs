70fc06d51be955299f78f6b2b43d91b1aab9757a
Pemdas <51948142+halloweenpandas@users.noreply.github.com>
2024-05-26 23:38:06 +0200

    Minor fixes (#31)
    * update BACType30ViewModel.cs
    
    * update BACType28 view
    
    * update label
    
    * Add BacType31 view
    
    * Fix skill type for Skill Upgrade function

---
+++ XenoKit/Views/BAC/BacType31View.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using XenoKit.ViewModel.BAC;
+using XenoKit.Controls;
+
+namespace XenoKit.Views.BAC
+{
+    /// <summary>
+    /// Interaction logic for BacType28View.xaml
+    /// </summary>
+    public partial class BacType31View : UserControl, INotifyPropertyChanged
+    {
+        #region NotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+
+        public static readonly DependencyProperty BacViewModelProperty = DependencyProperty.Register(
+            "BacViewModel", typeof(BACType31ViewModel), typeof(BacType31View), new PropertyMetadata(default(BACType31ViewModel)));
+
+        public BACType31ViewModel BacViewModel
+        {
+            get { return (BACType31ViewModel)GetValue(BacViewModelProperty); }
+            set
+            {
+                SetValue(BacViewModelProperty, value);
+                NotifyPropertyChanged("BacViewModel");
+            }
+        }
+
+        public BacType31View()
+        {
+            InitializeComponent();
+            BacTab.BacTypeSelectionChanged += BacTab_BacTypeSelectionChanged;
+            BacTab_BacTypeSelectionChanged(null, null);
+        }
+
+        private void BacTab_BacTypeSelectionChanged(object sender, EventArgs e)
+        {
+            if (BacViewModel != null)
+            {
+                Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Visibility = Visibility.Collapsed;
+            }
+
+        }
+    }
+}
